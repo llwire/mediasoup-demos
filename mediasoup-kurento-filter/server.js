f7e4416fa353789888db96124dc138c5491993b2
Lloyd Wiredu <lloyd.wiredu@shopify.com>
2020-05-03 17:11:03 -0400

    Update RTP

--- mediasoup-kurento-filter/server.js
+++ mediasoup-kurento-filter/server.js
@@ -962,9 +962,9 @@ async function startKurentoRtpProducer(enableSrtp) {
   global.kurento.rtc.sendEndpoint.connect(kmsRtpEndpoint);
 
   console.log("SDP Offer from App to Kurento RTP SEND:\n%s", kmsSdpOffer);
-  const kmsSdpAnswer = await global.kurento.rtp.sendEndpoint.processOffer(kmsSdpOffer);
+  const kmsSdpAnswer = await kmsRtpEndpoint.processOffer(kmsSdpOffer);
   console.log("SDP Answer from Kurento RTP SEND to App:\n%s", kmsSdpAnswer);
-  await kmsRtpEndpoint.processAnswer(kmsSdpAnswer);
+  await global.kurento.rtp.sendEndpoint.processAnswer(kmsSdpAnswer);
 
   // NOTE: A real application would need to parse this SDP Answer and adapt to
   // the parameters given in it, following the SDP Offer/Answer Model.
