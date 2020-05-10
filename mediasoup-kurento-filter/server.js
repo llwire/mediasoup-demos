c0746a1ca56543a6e305c26fd31da5d7a41aeb60
Lloyd Wiredu <lloyd.wiredu@shopify.com>
2020-05-10 03:40:23 -0400

    blah

--- mediasoup-kurento-filter/server.js
+++ mediasoup-kurento-filter/server.js
@@ -358,7 +358,7 @@ async function startKurentoRtpProducer(enableSrtp) {
     "";
 
   // Set maximum bitrate higher than default of 500 kbps
-  await kmsRtpEndpoint.setMaxVideoSendBandwidth(3000); // Send max 8mbps
+  await kmsRtpEndpoint.setMaxVideoSendBandwidth(2000); // Send max 8mbps
 
   console.log("SDP Offer from App to Kurento RTP SEND:\n%s", kmsSdpOffer);
   const kmsSdpAnswer = await kmsRtpEndpoint.processOffer(kmsSdpOffer);
@@ -390,8 +390,8 @@ function startGStreamerRtmpStream() {
     "--eos-on-shutdown",
     `filesrc location=${global.gstreamer.sdpFilesrc} !`,
     "sdpdemux name=sdpdm timeout=0",
-    "sdpdm.stream_0 ! queue ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! voaacenc ! mux.",
-    "sdpdm.stream_1 ! queue ! rtph264depay ! h264parse ! mux.",
+    "sdpdm.stream_0 ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! voaacenc ! mux.",
+    "sdpdm.stream_1 ! rtph264depay ! h264parse ! mux.",
     `flvmux name=mux streamable=true ! rtmpsink sync=false async=false location=${global.gstreamer.rtmpTarget}${testFlag}`,
   ].join(' ').trim();
 
