a58232a141213d3ce52d485a3f33998630db9cf8
Lloyd Wiredu <lloyd.wiredu@shopify.com>
2020-05-10 21:05:59 -0400

    blah

--- mediasoup-kurento-filter/server.js
+++ mediasoup-kurento-filter/server.js
@@ -412,7 +412,8 @@ function startGStreamerRtmpStream() {
     "sdpdm.stream_0 ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! voaacenc ! mux.",
     "sdpdm.stream_1 ! rtph264depay ! h264parse config-interval=2 ! mux.",
     `flvmux name=mux streamable=true !`,
-    `rtmpsink sync=false location="${global.gstreamer.rtmpTarget}${testFlag} live=1"`,
+    `rtmpsink sync=false location="${global.gstreamer.rtmpTarget}${testFlag} live=1" !`,
+    `progressreport name="RTMP Stream Progress" update-freq=10`,
   ].join(' ').trim();
 
   // avdec_h264 ! x264enc key-int-max=2
